c7c21c6a44717b47c309eff0a187828a4ada6f66
Garrett Aspacher <gma615@gmail.com>
2020-12-29 16:24:13 -0500

    Initial commit

---
+++ BasicMath/BasicMath/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BasicMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 15;
+            int num2 = 5;
+            int add = num1 + num2;
+            Console.WriteLine(add);
+            int subtract = num1 - num2;
+            Console.WriteLine(subtract);
+            int multiply = num1 * num2;
+            Console.WriteLine(multiply);
+            int divide = num1 / num2;
+            Console.WriteLine(divide);
+
+            string first = "Garrett";
+            string last = "Aspacher";
+            Console.WriteLine(first + last);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!\nMy name is Garrett");
+        Console.ReadLine();
+    }
+}
+
