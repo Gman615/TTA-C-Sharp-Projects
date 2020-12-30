cb3464e39f9dda7d3e84eb7adcadaa4b181ee4d4
Garrett Aspacher <gma615@gmail.com>
2020-12-30 11:15:21 -0500

    Update package express

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -14,7 +14,7 @@ namespace PackageExpress
             string weight = Console.ReadLine();
             int weight1 = Convert.ToInt32(weight);
             
-            if (weight1 < 50) 
+            if (weight1 <= 50) 
             {
                 Console.WriteLine("Please enter the package width:");
                 string width = Console.ReadLine();
@@ -25,8 +25,16 @@ namespace PackageExpress
                 Console.WriteLine("Please enter the package length:");
                 string length = Console.ReadLine();
                 int length1 = Convert.ToInt32(length);
-                int price = ((height1 * length1 * width1) * weight1 / 100);
-                Console.WriteLine("Your estimated total for shipping this package is: $" + price + "\nThank You");                                
+                int dimensions = height1 + length1 + width1;                
+                if (dimensions <= 50) 
+                {
+                    int price = (dimensions * weight1) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + price + "\nThank You");
+                }
+                else 
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
             }
             else 
             {
