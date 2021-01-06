66c78a2ef3b61be89e848e586c079f4d3a6d978a
Garrett Aspacher <gma615@gmail.com>
2021-01-05 20:54:31 -0700

    Added MethodMath

---
+++ TestMethods/TestMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 15;
+            double num2 = 6.33;
+            string str = "18";
+            int answer;
+            double answer1;
+            int answer2;
+            TestMethods math = new TestMethods();
+            answer = math.Math(num1);
+            Console.WriteLine(answer);
+            answer1 = math.Math(num2);
+            Console.WriteLine(answer1);
+            answer2 = math.Math(str);
+            Console.WriteLine(answer2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestMethods/TestMethods/TestMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMethods
+{
+    class TestMethods
+    {
+        public int Math(int num) 
+        {
+            return 10 * num;
+        }
+        public double Math(double num) 
+        {
+            return 7 * num;
+        }
+        public int Math(string str) 
+        {
+            int num = Convert.ToInt32(str) * 3;
+            return num;
+        }
+        
+    }
+}
