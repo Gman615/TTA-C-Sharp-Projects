b07b02a86e1c79d006e9821d6617d59dd6e75fef
Garrett Aspacher <gma615@gmail.com>
2021-01-06 12:55:20 -0700

    added VoidMethod

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMath voidMath = new VoidMath();
+            int num1 = 5;
+            int num2 = 10;
+            voidMath.VMath(num1, num2);
+            voidMath.VMath(10, 2);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/VoidMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class VoidMath
+    {
+        public void VMath(int num, int num1) 
+        {
+            int num2 = num * 5;            
+            Console.WriteLine(num1);
+        }
+    }
+}
