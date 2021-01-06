a7471ee5b13a60398973280ec9ac3616648b0377
Garrett Aspacher <gma615@gmail.com>
2021-01-06 12:25:51 -0700

    added OptParameters

---
+++ OptParameters/OptParameters/OptPara.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptParameters
+{
+    class OptPara
+    {
+        public int Equation(int num1, int num2 = 2) 
+        {
+            return 10 * num1 * num2;
+        }
+    }
+}
---
+++ OptParameters/OptParameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number for this math equation");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another number for this math equation (note: you do not have to pick anything)");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            int answer;
+            int answer1;
+            OptPara optpara = new OptPara();
+            if (num2 == 0)
+            {
+                answer = optpara.Equation(num1);
+                Console.WriteLine(answer);
+            }
+            else
+            {
+                answer1 = optpara.Equation(num1, num2);
+                Console.WriteLine(answer1);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
