1a45cf5948d7070fc39fbd0bd7fbc5368867c393
Garrett Aspacher <gma615@gmail.com>
2021-01-05 17:46:46 -0700

    added Method practice

---
+++ MethodMath/MethodMath/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMath
+{
+    class Math
+    {
+        public int Math1(int num) 
+        {
+            return 5 * num;
+        }
+        public int Math2(int num) 
+        {
+            return num / 2;
+        }
+        public int Math3(int num) 
+        {
+            return 15 + num;
+        }
+    }
+}
---
+++ MethodMath/MethodMath/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a random number for some math operations:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int answer;
+            Math math = new Math();
+
+
+            answer = math.Math1(num1);
+            Console.WriteLine(answer);
+            answer = math.Math2(num1);
+            Console.WriteLine(answer);
+            answer = math.Math3(num1);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }    
+}
