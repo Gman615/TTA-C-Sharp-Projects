351c629bde724fd3ca487d01abb7f984e2ab5763
Garrett Aspacher <gma615@gmail.com>
2021-01-07 18:49:24 -0700

    added StructPractice

---
+++ StructPractice/StructPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPractice
+{
+    class Program
+    {
+        public struct Number 
+        {
+            public decimal Amount { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 33.33333m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
