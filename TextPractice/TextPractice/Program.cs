8cb1bd1850e6166f7b4221abd3fe79410dd6fa9a
Garrett Aspacher <gma615@gmail.com>
2021-01-11 13:11:29 -0700

    adding text practice

---
+++ TextPractice/TextPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int result = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\gma61\source\repos\number.txt", true)) 
+            {
+                file.WriteLine(result);
+            }
+            Console.WriteLine("The number you entered is " + result);
+            Console.ReadLine();
+        }
+    }
+}
