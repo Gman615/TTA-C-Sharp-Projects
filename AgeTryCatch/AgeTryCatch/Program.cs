c6948a20ff2af1168902879dd454dae2036152fe
Garrett Aspacher <gma615@gmail.com>
2021-01-11 19:15:33 -0700

    added AgeTryCatch

---
+++ AgeTryCatch/AgeTryCatch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age? Please enter below");
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0) 
+                {
+                    throw new WrongNumber();
+                }
+                else Console.WriteLine("You are {0} years old!", age);
+                
+            }
+            catch (WrongNumber) 
+            {
+                Console.WriteLine("Negative numbers and 0 are not allowed");
+            }
+            catch (Exception) 
+            {
+                Console.WriteLine("An error occured, ending program");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AgeTryCatch/AgeTryCatch/WrongNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeTryCatch
+{
+    public class WrongNumber : Exception
+    {
+        public WrongNumber()
+            : base() { }
+        public WrongNumber(string message)
+            : base(message) { }
+    }
+}
